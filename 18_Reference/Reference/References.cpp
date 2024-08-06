baa3b5fb20c9cba87e0f911e5edaec6ee80f4304
Deny Febriyanto <denyfebri@gmail.com>
2024-08-06 22:21:02 +0700

    Belajar references

---
+++ 18_Reference/Reference/References.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//variable
+	int a = 5;
+
+	cout << "Address dari a " << &a << endl;
+	cout << "  Nilai dari a " << a << endl << endl;
+
+	//references
+	int& b = a;
+
+	cout << "  Nilai dari b " << b << endl;
+	cout << "Address dari b " << &b << endl << endl;
+
+	b = 10;
+	cout << "Nilai dari a " << a << endl;
+	cout << "Nilai dari b " << b << endl << endl;
+
+	a = 25;
+	cout << "Nilai dari a " << a << endl;
+	cout << "Nilai dari b " << b << endl << endl;
+}
